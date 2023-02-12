5fd4c69dcc17f4918019c3a97c6867acc69a7cd0
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-12 00:23:33 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// main.cpp
+#include <iostream>
+#include "funcs.h"
+
+int main() {
+std::cout << "Test 1: " << std::endl;
+print_interval(0, 9);
+std::cout << "Test 2: " << std::endl;
+print_interval(1, 10);
+std::cout << "Test 3: " << std::endl;
+print_interval(2, 11);
+  
+   return 0;
+}
