66e7ee6fda59a3a2370dbfe07d311d56d2028398
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-12 00:28:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include "funcs.h"
 
 int main() {
-std::cout << "Test 1: " << std::endl;
+std::cout << "\n Test 1:" <<  std::endl;
 print_interval(0, 9);
-std::cout << "Test 2: " << std::endl;
+std::cout << "\n Test 2:" << std::endl;
 print_interval(1, 10);
-std::cout << "Test 3: " << std::endl;
+std::cout << "\n Test 3:" << std::endl;
 print_interval(2, 11);
   
    return 0;
