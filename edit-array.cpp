0067239ab295fd21b7b7fbedefcdad1ae3143df3
Sarayaw <104982620+Sarayaw@users.noreply.github.com>
2023-02-11 23:18:25 -0500

    Update edit-array.cpp

--- edit-array.cpp
+++ edit-array.cpp
@@ -1,3 +1,13 @@
+/*
+Author: Saraya Weerasinghe
+Course: CSCI-135
+Instructor: Gennadiy Maryash
+Assignment: LAB 2C
+
+The user inputs an index and and a value and that value will replace anything in that index. If a user inputs an incorrect index, then the program will tell them that they didnt put the right index.
+*/
+
+
 #include <iostream>
 
 using namespace std;
